209fd6b999b8ea13a619a1eeb8db4add91431f15
Gökay <furkangokaypak@gmail.com>
2025-04-28 13:17:23 +0300

    Update V2

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -28,18 +28,16 @@ export default function Blog() {
         author={{
           name: person.name,
           url: `${baseURL}/blog`,
-          image: `${baseURL}${person.avatar}`,
         }}
       />
       <Heading marginBottom="l" variant="display-strong-s">
         {blog.title}
       </Heading>
-      <Column
-				fillWidth flex={1}>
-				<Posts range={[1,1]} thumbnail direction="column"/>
-				<Posts range={[2,3]} thumbnail/>
-				<Posts range={[4]} columns="2"/>
-			</Column>
+      <Column fillWidth flex={1}>
+        <Posts range={[1, 1]} thumbnail direction="column" />
+        <Posts range={[2, 3]} thumbnail />
+        <Posts range={[4]} columns="2" />
+      </Column>
       {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
